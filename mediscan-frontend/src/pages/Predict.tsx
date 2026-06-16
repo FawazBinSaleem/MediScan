f874b67df271fd39eace71034b7a917efef2c07a
FawazBinSaleem <fawaz.orton66@gmail.com>
2026-06-15 20:04:44 -0500

    feat: stores data after input

--- mediscan-frontend/src/pages/Predict.tsx
+++ mediscan-frontend/src/pages/Predict.tsx
@@ -1,22 +1,46 @@
 import { useParams } from "react-router-dom";
 import { diseaseForms } from "../data/DiseaseForms";
+import { useState } from "react";
 
 export default function Predict() {
   const { disease } = useParams();
 
+  const [formData, setFormData] = useState<Record<string, string>>({});
+
   const fields = diseaseForms[disease as keyof typeof diseaseForms];
+
+  function handleSubmit(event: React.FormEvent) {
+    event.preventDefault();
+
+    console.log(formData);
+  }
 
   return (
     <div>
       <h1>{disease} Prediction</h1>
 
-      {fields.map((field) => (
-        <div key={field.name}>
-          <label>{field.label}</label>
+      <form onSubmit={handleSubmit}>
+        {fields.map((field) => (
+          <div key={field.name}>
+            <label>{field.label}</label>
 
-          <input type={field.type} />
-        </div>
-      ))}
+            <input
+              type={field.type}
+              value={formData[field.name] || ""}
+              onChange={(e) =>
+                setFormData({
+                  ...formData,
+                  [field.name]: e.target.value,
+                })
+              }
+            />
+          </div>
+        ))}
+
+        <button type="submit">Predict</button>
+      </form>
+
+      <pre>{JSON.stringify(formData, null, 2)}</pre>
     </div>
   );
 }
