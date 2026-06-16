9f97df555c56e5e3d60b2fbeb1b712fddef590e6
FawazBinSaleem <fawaz.orton66@gmail.com>
2026-06-15 19:54:49 -0500

    chore: making links dynamic

--- mediscan-frontend/src/pages/Predict.tsx
+++ mediscan-frontend/src/pages/Predict.tsx
@@ -1,11 +1,22 @@
 import { useParams } from "react-router-dom";
+import { diseaseForms } from "../data/DiseaseForms";
 
 export default function Predict() {
   const { disease } = useParams();
 
+  const fields = diseaseForms[disease as keyof typeof diseaseForms];
+
   return (
     <div>
-      <h1>Predict: {disease}</h1>
+      <h1>{disease} Prediction</h1>
+
+      {fields.map((field) => (
+        <div key={field.name}>
+          <label>{field.label}</label>
+
+          <input type={field.type} />
+        </div>
+      ))}
     </div>
   );
 }
